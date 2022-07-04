6dc3b96fc87bd6dc0b4a87425b41dc3741de12c5
santosandressa <andressasantosp0@gmail.com>
2022-07-04 09:40:40 -0300

    task 0

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,29 @@
+public class Name {
+
+    public Date modifiedDate;
+
+    public List<Testcase> findAllTestcasesByUser(Name.User user){return null;}
+
+    public List<Account> AccountList;
+
+    public void deleteUser(Name.User user){}
+
+    public URL url;
+
+    public class TableRepresent {};
+    public void saveCar(){};
+
+    public saveUser(){}; saveAccount(){}; saveContract(){};
+
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+
+    public class User {
+    }
+
+    public class Testcase {
+    }
+
+    public class Account {}
+}
+
